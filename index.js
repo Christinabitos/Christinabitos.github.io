1a4738942dd4caa6ff788a4842567d7924034eb3
Christinabitos <bitostinaw@gmail.com>
2024-05-25 18:13:33 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const btnLike1 = document.getElementById("btnlike1")
+const countLike1 = document.getElementById("countlike1")
+function clickLike1(){
+     let totalLikes = countlike1.value + 1
+     countlike.textContent = totallikes.toString()
+}
